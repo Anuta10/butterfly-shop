0ea82f21535ba27aeb640f7bb5d304da261520e9
Anna Kolesova <kolesova-niusha91@yandex.ru>
2023-03-23 22:19:44 +0400

    add "faq" section + accordion, edit "reviews"

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -3,6 +3,23 @@ $(document).ready(function () {
   $('.j-hamburger').on('click', function () {
     $('.j-main-menu').toggleClass('is-open');
 
+  });
+
+
+  //Accordion//
+
+  let prevAccordionBtn = undefined;
+
+  $('.j-accordion-btn').on('click',function () {
+
+    if (prevAccordionBtn === this) {
+      $(this).next().slideToggle();
+    } else {
+      $(prevAccordionBtn).next().slideUp();
+      $(this).next().slideDown();
+      prevAccordionBtn = this;
+    }
+
   })
 
 })
